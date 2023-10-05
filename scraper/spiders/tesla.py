36ee1e00bcf8b8122a5d2772136788f4c09518e4
Issac Loo <issaloo@umich.edu>
2023-10-05 19:40:44 -0400

    feat(multiple): add tesla spider

--- scraper/spiders/tesla.py
+++ scraper/spiders/tesla.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 # from scrapy.selector
 
@@ -12,20 +13,29 @@ class TeslaSpider(scrapy.Spider):
 
     def start_requests(self):
         base_url = "http://www.tesla.com"
-        model_list = ["models", "model3", "modelx", "modely"]  # TODO: make this dynamic
+        model_list = ["model s", "model 3", "model x", "model y"]  # TODO: make this dynamic
         for model in model_list:
-            url = f"{base_url}/{model}"
+            url = f"{base_url}/{model.replace(' ', '')}"
             yield scrapy.Request(url=url, callback=self.parse, meta={"model": model})
 
     def parse(self, response):
-        text_list = response.xpath(
-            f'//p[contains(translate(@class, {self.uc}, {self.lc}), "disclaimer")]/text()'
-        ).getall()
-        dollar_list = ["$" in text.replace(",", "") for text in text_list]
-        if len(dollar_list) == 1:
-            dollar_list[0]
-            # TODO: extract dollar value using regex
-        else:
-            ValueError("Need single text extracted for dollar value.")
-        result = {"brand": "tesla", "model": self.meta.model}  # TODO: add meta
+        model = response.meta.get("model")
+        
+        # get dollar value
+        xpath_dollar_str = f'//p[contains(translate(@class, "{self.uc}", "{self.lc}"), "disclaimer")]/text()'
+        text_list = response.xpath(xpath_dollar_str).getall()
+        price = [text.replace(",", "") for text in text_list if "$" in text][0]
+        price = float(re.search(r"\b(?<=\$)\d+\b", price).group(0))
+    
+        # get image source
+        xpath_img_str = (f'//picture[contains(translate(@data-alt, "{self.uc}", "{self.lc}"), "{model}") ' 
+                            f'and contains(translate(@data-alt, "{self.uc}", "{self.lc}"), "side view")]/'
+                            f'@data-iesrc[contains(translate(., "{self.uc}", "{self.lc}"), "order")]')
+        img_src = response.xpath(xpath_img_str).get()
+
+        # yield data
+        result = {"brand": "tesla", 
+                  "model": model,
+                  "price": price,
+                  "image_src": img_src}
         yield result
